66e7dab5aaca7f35405454765491e648b5053c71
Chinmay D. Pai <chinmaydpai@gmail.com>
2024-09-22 15:06:11 +0530

    chore: update netbird to 0.29.4
    Signed-off-by: Chinmay D. Pai <chinmaydpai@gmail.com>

--- overlays/netbird/default.nix
+++ overlays/netbird/default.nix
@@ -1,9 +1,9 @@
 _: _self: super: let
-  version = "0.28.8";
+  version = "0.29.4";
 in {
   netbird = super.netbird.override {
     buildGoModule = args:
-      super.buildGoModule (
+      super.buildGo123Module (
         args
         // {
           inherit version;
@@ -11,9 +11,9 @@ in {
             owner = "netbirdio";
             repo = "netbird";
             rev = "v${version}";
-            hash = "sha256-DfY8CVBHgE/kLALKNzSgmUxM0flWLesU0XAgVsHHLKc=";
+            hash = "sha256-W71CE83r/RcPdBjzfT+FSh72UcKcTmIuagkrC1eaeNk=";
           };
-          vendorHash = "sha256-CqknRMijAkWRLXCcIjRBX2wB64+RivD/mXq28TqzNjg=";
+          vendorHash = "sha256-CD34U+Z8bUKN0Z4nxIVC+mYDp71Q8q1bmUypRDGgb3U=";
           ldflags = [
             "-s"
             "-w"
