fed120b28b16f59cce6870c905c3adb287c7fe8f
Chinmay D. Pai <chinmaydpai@gmail.com>
2024-10-06 00:08:12 +0530

    feat: add jellyfin-web overlay with intro-skipper-plugin support
    Signed-off-by: Chinmay D. Pai <chinmaydpai@gmail.com>

---
+++ overlays/jellyfin-web/default.nix
@@ -0,0 +1,17 @@
+# Add support for intro-skipper-button
+# Ref: https://wiki.nixos.org/wiki/Jellyfin#Intro_Skipper_plugin
+_: _self: super: {
+  jellyfin-web = super.jellyfin-web.overrideAttrs (finalAttrs: previousAttrs: {
+    installPhase = ''
+      runHook preInstall
+
+      # this is the important line
+      sed -i "s#</head>#<script src=\"configurationpage?name=skip-intro-button.js\"></script></head>#" dist/index.html
+
+      mkdir -p $out/share
+      cp -a dist $out/share/jellyfin-web
+
+      runHook postInstall
+    '';
+  });
+}
