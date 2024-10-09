de0f3ae50f81fe4f6705158ead4a0dac4157b6be
Chinmay D. Pai <chinmaydpai@gmail.com>
2024-10-09 17:57:50 +0530

    chore: remove unused comment for git module
    Signed-off-by: Chinmay D. Pai <chinmaydpai@gmail.com>

--- homes/x86_64-linux/server@bicboye/default.nix
+++ homes/x86_64-linux/server@bicboye/default.nix
@@ -2,7 +2,6 @@ _: {
   snowfallorg.user.enable = true;
   snowfallorg.user.name = "server";
 
-  # snowflake.development.git.enable = true;
   snowflake.development.helix.enable = true;
   snowflake.development.tmux.enable = true;
   snowflake.shell.fish.enable = true;
