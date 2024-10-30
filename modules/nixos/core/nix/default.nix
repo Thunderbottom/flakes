6a7f73ec39d0b96b2b727845cb6289586367d330
Chinmay D. Pai <chinmaydpai@gmail.com>
2024-10-31 00:47:29 +0530

    chore: switch nix back to nix_git, add cachix for helix
    Signed-off-by: Chinmay D. Pai <chinmaydpai@gmail.com>

--- modules/nixos/core/nix/default.nix
+++ modules/nixos/core/nix/default.nix
@@ -27,10 +27,7 @@
       nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
 
       # Use the latest, unstable version of nix.
-      package = pkgs.nixVersions.nix_2_23;
-      # TODO: switch back to nix git. Current version has a security issue that allows
-      # remote code execution.
-      # package = pkgs.nixVersions.git;
+      package = pkgs.nixVersions.git;
 
       settings = {
         # Accept flake configuration without prompting.
@@ -72,10 +69,12 @@
         trusted-substituters = [
           "https://nix-community.cachix.org"
           "https://hyprland.cachix.org"
+          "https://helix.cachix.org"
         ];
         trusted-public-keys = [
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
           "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+          "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
         ];
       };
     };
