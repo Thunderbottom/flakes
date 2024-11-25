cf042b4d3118a6fe28eb56322b86a8779a78c1a0
Chinmay D. Pai <chinmaydpai@gmail.com>
2024-11-26 00:47:57 +0530

    feat: add service for asusd and supergfxd
    Prepare for setting up zephyrus g14 on nixos
    
    Signed-off-by: Chinmay D. Pai <chinmaydpai@gmail.com>

---
+++ modules/nixos/services/asus/default.nix
@@ -0,0 +1,21 @@
+{
+  config,
+  lib,
+  ...
+}: {
+  options.snowflake.services.asus.enable =
+    lib.mkEnableOption "Enable Asus-specific configuration";
+
+  config = lib.mkIf config.snowflake.services.asus.enable {
+    # specific to Asus laptop
+    # already included in flake.nix from https://github.com/NixOS/nixos-hardware/blob/master/asus/zephyrus/ga402x/shared.nix still overwiting it
+    # source: https://asus-linux.org/guides/nixos/
+    services = {
+      supergfxd.enable = true;
+      asusd = {
+        enable = true;
+        enableUserService = true;
+      };
+    };
+  };
+}
