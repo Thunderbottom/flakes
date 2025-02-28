7f1659c8e9fbc165ff61cd67e3c72b97bcdc2cce
Chinmay D. Pai <chinmaydpai@gmail.com>
2025-02-28 14:24:32 +0530

    feat: add support for bluetooth LE, disable on boot
    Signed-off-by: Chinmay D. Pai <chinmaydpai@gmail.com>

--- modules/nixos/hardware/bluetooth/default.nix
+++ modules/nixos/hardware/bluetooth/default.nix
@@ -2,6 +2,7 @@
   config,
   lib,
   namespace,
+  pkgs,
   ...
 }: {
   options.${namespace}.hardware.bluetooth.enable = lib.mkEnableOption "Enable bluetooth hardware support";
@@ -10,13 +11,18 @@
     # Enable bluetooth hardware.
     hardware.bluetooth = {
       enable = true;
+      powerOnBoot = false;
       settings = {
         General = {
+          Enable = "Source,Sink,Media,Socket";
           # Enable battery charge levels for bluetooth devices.
           Experimental = true;
           KernelExperimental = true;
         };
       };
     };
+
+    # Add support for Bluetooth LE
+    ${namespace}.extraPackages = [pkgs.liblc3];
   };
 }
