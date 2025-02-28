1d6e163af99acaf8e427b89c4d5fc307979b7abd
Chinmay D. Pai <chinmaydpai@gmail.com>
2025-02-28 10:16:48 +0530

    chore: switch thonkpad from wezterm to ghostty
    Signed-off-by: Chinmay D. Pai <chinmaydpai@gmail.com>

--- homes/x86_64-linux/chnmy@thonkpad/default.nix
+++ homes/x86_64-linux/chnmy@thonkpad/default.nix
@@ -6,8 +6,9 @@
   snowfallorg.user.enable = true;
   snowfallorg.user.name = "chnmy";
 
-  snowflake.desktop.wezterm.enable = true;
   snowflake.desktop.firefox.enable = true;
+  snowflake.desktop.ghostty.enable = true;
+  snowflake.desktop.gnome-dconf.enable = true;
 
   snowflake.development.git.enable = true;
   snowflake.development.git.user = {
