bce71b50fb1fc9921efafb28e402fbe8313920ac
Chinmay D. Pai <chinmaydpai@gmail.com>
2025-04-27 12:47:42 +0530

    chore: use overrideAttrs for netbird overlay and update to 0.43.0
    `override` changed the entire build struct and the dependencies could break with
    future upgrades, whereas `overrideAttrs` only updates the specific attributes
    listed in the overlay. For our use case, `overrideAttrs` is good enough
    
    Signed-off-by: Chinmay D. Pai <chinmaydpai@gmail.com>

--- overlays/netbird/default.nix
+++ overlays/netbird/default.nix
@@ -1,26 +1,25 @@
-_: _self: super: let
-  version = "0.37.1";
-in {
-  netbird = super.netbird.override {
-    buildGoModule = args:
-      super.buildGo123Module (
-        args
-        // {
-          inherit version;
-          src = super.fetchFromGitHub {
-            owner = "netbirdio";
-            repo = "netbird";
-            rev = "v${version}";
-            hash = "sha256-5+R0Y/xPgnVH53p1vtY65tOqePWQVOMR4oY1yOOFHK4=";
-          };
-          vendorHash = "sha256-DGvDkkdM8WaaR5FQwZgKn2n1JEDeqUegZxeAIxniJ5A=";
-          ldflags = [
-            "-s"
-            "-w"
-            "-X github.com/netbirdio/netbird/version.version=${version}"
-            "-X main.builtBy=nix"
-          ];
-        }
-      );
-  };
+_: _self: super:
+let
+  # version = "0.37.1";
+  version = "0.43.0";
+in
+{
+  netbird = super.netbird.overrideAttrs (old: {
+    inherit version;
+    src = super.fetchFromGitHub {
+      owner = "netbirdio";
+      repo = "netbird";
+      rev = "v${version}";
+      hash = "sha256-HmNd5MyplQ8iwpaxhEnomASIwx4VE3Qv70sURxqDzdo=";
+      # hash = "sha256-5+R0Y/xPgnVH53p1vtY65tOqePWQVOMR4oY1yOOFHK4=";
+    };
+    vendorHash = "sha256-/iqWVDqQOTFP5OZDrgq5gAH7NmHneQlf5+wAzIyoEPw=";
+    # vendorHash = "sha256-DGvDkkdM8WaaR5FQwZgKn2n1JEDeqUegZxeAIxniJ5A=";
+    ldflags = [
+      "-s"
+      "-w"
+      "-X github.com/netbirdio/netbird/version.version=${version}"
+      "-X main.builtBy=nix"
+    ];
+  });
 }
