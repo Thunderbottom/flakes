77f25704cb7c91f008b4838925e77ce66e1619aa
Chinmay D. Pai <chinmaydpai@gmail.com>
2025-05-17 15:30:06 +0530

    chore: remove unused desktop variables
    Signed-off-by: Chinmay D. Pai <chinmaydpai@gmail.com>

--- modules/nixos/desktop/default.nix
+++ modules/nixos/desktop/default.nix
@@ -4,7 +4,8 @@
   namespace,
   pkgs,
   ...
-}: {
+}:
+{
   options.${namespace}.desktop = {
     enable = lib.mkEnableOption "Enable core Desktop Environment configuration";
     fingerprint.enable = lib.mkEnableOption "Enable fingerprint support for Desktop Environments";
@@ -22,7 +23,7 @@
 
       # Add user to networkmanager and adbusers group.
       # Works only when ${namespace}.user.enable is true.
-      user.extraGroups = ["adbusers"];
+      user.extraGroups = [ "adbusers" ];
     };
 
     # Enable ADB for Android devices.
@@ -44,11 +45,11 @@
     services.xserver.enable = true;
     # Prevents xterm from being installed.
     # Prefer installing a custom terminal emulator instead.
-    services.xserver.excludePackages = [pkgs.xterm];
+    services.xserver.excludePackages = [ pkgs.xterm ];
     services.xserver.desktopManager.xterm.enable = false;
 
     # Add udev rules for ADB.
-    services.udev.packages = [pkgs.android-udev-rules];
+    services.udev.packages = [ pkgs.android-udev-rules ];
 
     # Enable XDG Portal.
     # Additional configuration will be done through individual
@@ -59,28 +60,24 @@
     xdg.portal.xdgOpenUsePortal = true;
 
     # Add bibata cursors everywhere because it looks cool.
-    environment.systemPackages = [pkgs.bibata-cursors];
+    environment.systemPackages = [ pkgs.bibata-cursors ];
 
     # Set environment variables for the system.
     environment.variables = {
       # Make Electron applications run in wayland.
       NIXOS_OZONE_WL = "1";
       # Disable G-Sync and VRR.
-      __GL_GSYNC_ALLOWED = "0";
-      __GL_VRR_ALLOWED = "0";
       GDK_BACKEND = "wayland";
       DIRENV_LOG_FORMAT = "";
-      WLR_DRM_NO_ATOMIC = "1";
       QT_AUTO_SCREEN_SCALE_FACTOR = "1";
       QT_QPA_PLATFORM = "wayland;xcb";
-      WLR_BACKEND = "vulkan";
-      WLR_RENDERER = "vulkan";
       XDG_SESSION_TYPE = "wayland";
       SDL_VIDEODRIVER = "wayland";
       CLUTTER_BACKEND = "wayland";
 
       GST_PLUGIN_SYSTEM_PATH_1_0 = lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" (
-        with pkgs.gst_all_1; [
+        with pkgs.gst_all_1;
+        [
           gstreamer
           gst-plugins-base
           gst-plugins-good
