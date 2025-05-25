d5a7b07d8808384adc40a4e9f00191376db3e6e5
Chinmay D. Pai <chinmaydpai@gmail.com>
2025-05-25 10:23:02 +0530

    feat: add amdvlk driver for amdgpu
    Signed-off-by: Chinmay D. Pai <chinmaydpai@gmail.com>

--- modules/nixos/hardware/graphics/amd/default.nix
+++ modules/nixos/hardware/graphics/amd/default.nix
@@ -2,9 +2,12 @@
   config,
   lib,
   namespace,
+  pkgs,
   ...
-}: {
-  options.${namespace}.hardware.graphics.amd.enable = lib.mkEnableOption "Enable AMD graphics configuration";
+}:
+{
+  options.${namespace}.hardware.graphics.amd.enable =
+    lib.mkEnableOption "Enable AMD graphics configuration";
 
   config = lib.mkIf config.${namespace}.hardware.graphics.amd.enable {
     hardware.amdgpu.initrd.enable = lib.mkDefault true;
@@ -13,10 +16,16 @@
     hardware.graphics = {
       enable = true;
       enable32Bit = true;
+      extraPackages = with pkgs; [
+        amdvlk
+      ];
     };
 
-    environment.sessionVariables.LIBVA_DRIVER_NAME = "radeonsi";
+    environment.sessionVariables = {
+      LIBVA_DRIVER_NAME = "radeonsi";
+      RADV_PERFTEST = "gpl";
+    };
 
-    boot.initrd.kernelModules = ["amdgpu"];
+    boot.initrd.kernelModules = [ "amdgpu" ];
   };
 }
