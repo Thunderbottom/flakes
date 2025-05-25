821e67db1c506b9ec8e684b81cd458f51635ea50
Chinmay D. Pai <chinmaydpai@gmail.com>
2025-05-25 10:22:33 +0530

    fix: improve truetype font rendering
    Signed-off-by: Chinmay D. Pai <chinmaydpai@gmail.com>

--- modules/nixos/desktop/default.nix
+++ modules/nixos/desktop/default.nix
@@ -74,6 +74,7 @@
       XDG_SESSION_TYPE = "wayland";
       SDL_VIDEODRIVER = "wayland";
       CLUTTER_BACKEND = "wayland";
+      FREETYPE_PROPERTIES = "truetype:interpreter-version=40";
 
       GST_PLUGIN_SYSTEM_PATH_1_0 = lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" (
         with pkgs.gst_all_1;
