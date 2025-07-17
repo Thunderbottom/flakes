f10df8dbe6b477d40ae02d2e018b66b1f692755b
Chinmay D. Pai <chinmaydpai@gmail.com>
2025-07-18 01:12:29 +0530

    chore: remove unused overlays
    Signed-off-by: Chinmay D. Pai <chinmaydpai@gmail.com>

--- overlays/lttng-tools/default.nix
+++
@@ -1,5 +0,0 @@
-_final: prev: {
-  lttng-tools = prev.lttng-tools.overrideAttrs (_old: {
-    patches = [ ./fix-build.patch ];
-  });
-}
--- overlays/paperless-ngx/default.nix
+++
@@ -1,8 +0,0 @@
-_final: prev: {
-  paperless-ngx = prev.paperless-ngx.overrideAttrs (_old: {
-    disabledTests = _old.disabledTests ++ [
-      "test_favicon_view"
-      "test_favicon_view_missing_file"
-    ];
-  });
-}
