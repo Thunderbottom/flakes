cab822a9a4edfe44ff5cf0476938e73ee9127f0c
Chinmay D. Pai <chinmaydpai@gmail.com>
2025-07-17 22:29:17 +0530

    chore: add extraPackages for amd graphics
    Signed-off-by: Chinmay D. Pai <chinmaydpai@gmail.com>

--- modules/nixos/hardware/graphics/amd/default.nix
+++ modules/nixos/hardware/graphics/amd/default.nix
@@ -16,6 +16,15 @@
       enable = true;
       enable32Bit = true;
       extraPackages = with pkgs; [
+        libva
+        vaapiVdpau
+        libvdpau-va-gl
+        amdvlk
+        mesa
+      ];
+      extraPackages32 = with pkgs.pkgsi686Linux; [
+        vaapiVdpau
+        libvdpau-va-gl
         amdvlk
       ];
     };
