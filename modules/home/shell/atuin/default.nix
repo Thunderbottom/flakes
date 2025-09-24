bb232cffb76eaba1f8f27a7ce9e3a44bac3844ab
Chinmay D. Pai <chinmaydpai@gmail.com>
2025-09-24 11:56:17 +0530

    feat: add home shell module for atuin
    Signed-off-by: Chinmay D. Pai <chinmaydpai@gmail.com>

---
+++ modules/home/shell/atuin/default.nix
@@ -0,0 +1,33 @@
+{
+  config,
+  lib,
+  ...
+}:
+{
+  options.snowflake.shell.atuin = {
+    enable = lib.mkEnableOption "Enable atuin shell home configuration";
+    sync = {
+      address = lib.mkOption {
+        type = lib.types.str;
+        description = "Host for the atuin sync server";
+      };
+      frequency = lib.mkOption {
+        type = lib.types.str;
+        description = "Sync frequency with the atuin sync server";
+        default = "15m";
+      };
+    };
+  };
+
+  config = lib.mkIf config.snowflake.shell.atuin.enable {
+    programs.atuin = {
+      enable = true;
+      settings = {
+        sync_address = config.snowflake.shell.atuin.sync.address;
+        sync_frequency = config.snowflake.shell.atuin.sync.frequency;
+        dialect = "uk";
+      };
+      enableFishIntegration = config.snowflake.shell.fish.enable;
+    };
+  };
+}
--- users/chnmy/home.nix
+++ users/chnmy/home.nix
@@ -13,6 +13,10 @@
 
   snowflake.development.helix.enable = true;
   snowflake.development.tmux.enable = true;
+  snowflake.shell.atuin = {
+    enable = true;
+    sync.address = "https://atuin.deku.moe";
+  };
   snowflake.shell.fish.enable = true;
   snowflake.shell.direnv.enable = true;
 
