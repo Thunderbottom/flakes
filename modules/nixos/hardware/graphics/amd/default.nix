96920dba03f431db1a3485bc3d8fceaef4a746b6
Chinmay D. Pai <chinmaydpai@gmail.com>
2025-10-12 22:53:49 +0530

    chore: remove old amdvlk packages
    Signed-off-by: Chinmay D. Pai <chinmaydpai@gmail.com>

--- modules/nixos/hardware/graphics/amd/default.nix
+++ modules/nixos/hardware/graphics/amd/default.nix
@@ -19,13 +19,11 @@
         libva
         vaapiVdpau
         libvdpau-va-gl
-        amdvlk
         mesa
       ];
       extraPackages32 = with pkgs.pkgsi686Linux; [
         vaapiVdpau
         libvdpau-va-gl
-        amdvlk
       ];
     };
 
