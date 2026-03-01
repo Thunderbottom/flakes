bc36f6fc594ea68b845dc8c4b9448b32d351e316
Chinmay D. Pai <chinmaydpai@gmail.com>
2026-03-01 10:53:47 +0100

    chore: set up initrd networking for systemd ssh
    Signed-off-by: Chinmay D. Pai <chinmaydpai@gmail.com>

--- modules/nixos/hardware/initrd-luks/default.nix
+++ modules/nixos/hardware/initrd-luks/default.nix
@@ -40,22 +40,31 @@
     # $ ssh root@<ip>
     # While in the shell, run `systemctl default` or `systemd-ask-password`
     # to trigger the unlock prompt.
-    boot = {
-      initrd.network = {
-        flushBeforeStage2 = true;
+    boot.initrd = {
+      # Required for the network card that requires a kernel module to work.
+      inherit (config.snowflake.hardware.initrd-luks) availableKernelModules;
+
+      # Systemd networkd DHCP config (required when systemd is in initrd)
+      systemd.network.networks."10-initrd-wan" = lib.mkIf config.boot.initrd.systemd.enable {
+        matchConfig.Name = "enp2s0";
+        networkConfig.DHCP = "yes";
+      };
+
+      # Network and SSH config (works with both scripted and systemd initrd)
+      network = {
         enable = true;
+        flushBeforeStage2 = true;
+        # udhcpc only needed for scripted initrd (not systemd)
+        udhcpc.enable = lib.mkIf (!config.boot.initrd.systemd.enable) true;
         ssh = {
           enable = true;
           port = config.snowflake.hardware.initrd-luks.sshPort;
-          inherit (config.snowflake.hardware.initrd-luks) hostKeys;
-          inherit (config.snowflake.hardware.initrd-luks) authorizedKeys;
+          inherit (config.snowflake.hardware.initrd-luks) hostKeys authorizedKeys;
         };
       };
-      # Required for the network card that
-      # requires a kernel module to work.
-      initrd.availableKernelModules = config.snowflake.hardware.initrd-luks.availableKernelModules;
-      # Use DHCP to figure out the IP address.
-      kernelParams = [ "ip=dhcp" ];
     };
+
+    # Use DHCP kernel parameter only for scripted initrd (systemd ignores it)
+    boot.kernelParams = lib.mkIf (!config.boot.initrd.systemd.enable) [ "ip=dhcp" ];
   };
 }
