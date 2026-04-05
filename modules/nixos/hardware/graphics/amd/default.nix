d7095425d2ea31f5cb5b6bf04f472580ca167642
Chinmay D. Pai <chinmaydpai@gmail.com>
2026-04-05 15:56:15 +0200

    chore: update amd and nvidia driver config
    Signed-off-by: Chinmay D. Pai <chinmaydpai@gmail.com>

--- modules/nixos/hardware/graphics/amd/default.nix
+++ modules/nixos/hardware/graphics/amd/default.nix
@@ -27,17 +27,7 @@
       ];
     };
 
-    environment.sessionVariables = {
-      LIBVA_DRIVER_NAME = "radeonsi";
-      RADV_PERFTEST = "gpl,nggc,sam";
-      AMD_VULKAN_ICD = "RADV";
-    };
-
     services.xserver.videoDrivers = [ "amdgpu" ];
     boot.initrd.kernelModules = [ "amdgpu" ];
-
-    boot.kernelParams = [
-      "amdgpu.ppfeaturemask=0xffffffff"
-    ];
   };
 }
